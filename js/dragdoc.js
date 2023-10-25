4bfe416eadeb721ca48cd4efae5a0813705dd015
Andy Carlson <bng44270@gmail.com>
2023-10-25 12:01:16 -0500

    updated dragdoc to use wheel scrolling

--- js/dragdoc.js
+++ js/dragdoc.js
@@ -15,34 +15,34 @@ Usage:
 */
 
 class DragDoc {
-  constructor(id,imgUrl,containerStyle={},imgStyle={}) {
+  constructor(id, imgUrl, containerStyle = {}, imgStyle = {}) {
     this.DOCID = id;
-    
+
     this.initView(imgUrl);
-			
+
     var container = document.getElementById(this.DOCID + '_container');
     Object.keys(containerStyle).forEach(s => {
       container.style[s] = containerStyle[s];
     });
-    
+
     var content = document.getElementById(this.DOCID + '_content');
     Object.keys(imgStyle).forEach(s => {
       content.style[s] = imgStyle[s];
     });
-    
-    if (content.style.transform.length  == 0) {
+
+    if (content.style.transform.length == 0) {
       content.style.transform = 'scale(1)';
     }
-    
-    var zoomVal = content.style.transform.replace(/scale\(([0-9\.]+)\)/,'$1');
-      
-      var zoomControl = document.getElementById(this.DOCID + '_zoomcontrol');
-      var zoomDisplay = document.getElementById(this.DOCID + '_zoomdisplay');
 
-      zoomControl.value = zoomVal;
+    var zoomVal = content.style.transform.replace(/scale\(([0-9\.]+)\)/, '$1');
 
-      zoomDisplay.textContent = parseInt(parseFloat(zoomVal)*100).toString() + '%';
-    
+    var zoomControl = document.getElementById(this.DOCID + '_zoomcontrol');
+    var zoomDisplay = document.getElementById(this.DOCID + '_zoomdisplay');
+
+    zoomControl.value = zoomVal;
+
+    zoomDisplay.textContent = parseInt(parseFloat(zoomVal) * 100).toString() + '%';
+
 
     this.mouseDown = false;
     this.pos = { top: 0, left: 0, x: 0, y: 0 };
@@ -50,34 +50,35 @@ class DragDoc {
 
   initView(imgUrl) {
     const containerStyle = {
-      position : 'relative',
-      overflow : 'hidden',
-      clear : 'both'
+      position: 'relative',
+      overflow: 'hidden',
+      clear: 'both'
     };
 
     const contentStyle = {
-      cursor : 'grab',
-      overflow : 'hidden',
-      position : 'absolute',
-      'top' : '0px',
-      left : '0px'
+      cursor: 'grab',
+      overflow: 'hidden',
+      position: 'absolute',
+      'top': '0px',
+      left: '0px'
 
     };
 
     var image = document.createElement('img');
     image.id = this.DOCID + '_image';
-    image.setAttribute('draggable','false');
+    image.setAttribute('draggable', 'false');
     image.src = imgUrl;
     image.style.userSelect = 'none';
+    image.addEventListener('wheel', this.zoomHandler.bind(this), false);
 
     var content = document.createElement('div');
     content.id = this.DOCID + '_content';
     Object.keys(contentStyle).forEach(s => {
       content.style[s] = contentStyle[s];
     });
-    content.addEventListener('mousedown',this.mouseDownHandler.bind(this),false);
-    content.addEventListener('mousemove', this.mouseMoveHandler.bind(this),false);
-    content.addEventListener('mouseup', this.mouseUpHandler.bind(this),false);
+    content.addEventListener('mousedown', this.mouseDownHandler.bind(this), false);
+    content.addEventListener('mousemove', this.mouseMoveHandler.bind(this), false);
+    content.addEventListener('mouseup', this.mouseUpHandler.bind(this), false);
     content.appendChild(image);
 
     var container = document.createElement('div');
@@ -86,10 +87,10 @@ class DragDoc {
       container.style[s] = containerStyle[s];
     });
     container.appendChild(content);
-    
+
     var rootContainer = document.getElementById(this.DOCID);
     rootContainer.appendChild(container);
-    
+
     var zoomDisplay = document.createElement('span');
     zoomDisplay.id = this.DOCID + '_zoomdisplay';
     rootContainer.append(zoomDisplay);
@@ -100,7 +101,7 @@ class DragDoc {
     zoomControl.min = '0.1';
     zoomControl.max = '4';
     zoomControl.step = '0.1';
-    zoomControl.addEventListener('input',this.zoomHandler.bind(this),false);
+    zoomControl.addEventListener('input', this.zoomHandler.bind(this), false);
     rootContainer.append(zoomControl);
   }
 
@@ -108,29 +109,29 @@ class DragDoc {
     if (!this.mouseDown) {
       var content = document.getElementById(this.DOCID + '_content');
 
-      var left = parseInt(content.style.left.replace(/px$/,''));
-      var top = parseInt(content.style.top.replace(/px$/,''));
-      
+      var left = parseInt(content.style.left.replace(/px$/, ''));
+      var top = parseInt(content.style.top.replace(/px$/, ''));
+
       this.pos = {
-          // The current scroll
-          left: left,
-          top: top,
-          // Get the current mouse position
-          x: e.clientX,
-          y: e.clientY,
+        // The current scroll
+        left: left,
+        top: top,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
       };
 
       content.style.cursor = 'grabbing';
-    
+
       this.mouseDown = true;
     }
   }
-  
+
   mouseMoveHandler(e) {
     if (this.mouseDown) {
       const dx = e.clientX - this.pos.x;
       const dy = e.clientY - this.pos.y;
-  
+
       var content = document.getElementById(this.DOCID + '_content');
 
       // Scroll the element
@@ -138,13 +139,13 @@ class DragDoc {
       content.style.left = (this.pos.left + dx).toString() + "px";
     }
   }
-  
+
   mouseUpHandler(e) {
     if (this.mouseDown) {
       var content = document.getElementById(this.DOCID + '_content');
 
       content.style.cursor = 'grab';
-      
+
       this.mouseDown = false;
     }
   }
@@ -152,11 +153,26 @@ class DragDoc {
   zoomHandler(e) {
     var zoomDisplay = document.getElementById(this.DOCID + '_zoomdisplay');
     var content = document.getElementById(this.DOCID + '_content');
+    var zoomControl = document.getElementById(this.DOCID + '_zoomcontrol');
+    var image = document.getElementById(this.DOCID + '_image');
 
-    var zoomValue = e.target.value;
+    if (e.type == 'input' && e.target.id == zoomControl.id) {
+      var zoomValue = e.target.value;
 
-    content.style.transform = 'scale(' + zoomValue + ')';
+      content.style.transform = 'scale(' + zoomValue + ')';
 
-    zoomDisplay.textContent = parseInt(parseFloat(zoomValue)*100).toString() + '%';
+      zoomDisplay.textContent = parseInt(parseFloat(zoomValue) * 100).toString() + '%';
+    }
+    else if (e.type == 'wheel' && e.target.id == image.id) {
+      var zoomValue = parseFloat(content.style.transform.replace(/scale\(([0-9\.]+)\)/, '$1'));
+      
+      zoomValue = (e.deltaY > 0) ? zoomValue + 0.1 : zoomValue - 0.1;
+
+      if (zoomValue > 0 && zoomValue < 4.1) {
+        content.style.transform = 'scale(' + zoomValue.toString() + ')';
+        zoomDisplay.textContent = parseInt(parseFloat(zoomValue) * 100).toString() + '%';
+        zoomControl.value = zoomValue.toString();
+      }
+    }
   }
 }
